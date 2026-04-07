ce26bc4af526c74335324464df7ee35d4988ad3b
Daniellamm <57009827+Daniellamm@users.noreply.github.com>
2026-04-07 15:01:58 -0400

    Build fix

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,8 @@
 import typescript from 'rollup-plugin-typescript2';
-import commonjs from 'rollup-plugin-commonjs';
-import nodeResolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
-import { terser } from 'rollup-plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
 import json from '@rollup/plugin-json';
-
-const plugins = [
-  nodeResolve({}),
-  commonjs(),
-  typescript(),
-  json(),
-  babel({
-    exclude: 'node_modules/**',
-  }),
-  terser(),
-];
 
 export default [
   {
@@ -23,6 +11,12 @@ export default [
       dir: 'dist',
       format: 'es',
     },
-    plugins: [...plugins],
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+      typescript(),
+      json(),
+      terser(),
+    ],
   },
 ];
